6ca5e099e16a3a1571c72fe81e39ad92ed62895f
Gauthier Leonard <gauthier@adaltas.com>
2022-10-11 13:53:29 +0200

    feat: add setup script + update readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # TDP Monitoring
+
+## Using in tdp-getting-started
+
+```sh
+cd /path/to/tdp-getting-started
+# Make sure you are in TDP lib venv
+source ./venv/bin/activate
+
+# Install collection (SSH example)
+git clone git@github.com:alliage-io/tdp-monitoring.git ansible_collections/alliage/tdp_monitoring
+
+# Setup collection
+## Destructive setup: this will reset tdp-lib database
+./ansible_collections/alliage/tdp_monitoring/scripts/setup.sh -c
+## Non destructive: Manually add extra tdp_cluster variables to tdp_vars
+## ON FIRST INSTALL ONLY
+cat ansible_collections/alliage/tdp_monitoring/tdp_vars_defaults/tdp_cluster/tdp_cluster.yml |
+grep -vP '^[#-]' | grep -P '^\w+' >> inventory/tdp_vars/tdp_cluster/tdp_cluster.yml
+cd inventory/tdp_vars/tdp_cluster && git commit -a -m 'feat: add tdp_monitoring vars' && cd -
+./ansible_collections/alliage/tdp_monitoring/scripts/setup.sh
+
+# Deploy Prometheus
+tdp deploy --targets prometheus_start
+```
+
+## Web UI links
+
+- Prometheus targets: https://master-01.tdp:9090/targets
+  - Username: `admin`
+  - Password: `PrometheusAdmin123`
---
+++ scripts/setup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Copyright 2022 Adaltas
+# SPDX-License-Identifier: MIT
+
+CLEAN="false"
+
+parse_cmdline() {
+  local OPTIND
+  while getopts 'c' options; do
+    case "$options" in
+    c) CLEAN="true" ;;
+    *) return 1 ;;
+    esac
+  done
+  shift $((OPTIND - 1))
+  return 0
+}
+
+create_symlink_if_needed() {
+  local target=$1
+  local link_name=$2
+  if [[ "$CLEAN" == "true" ]]; then
+    echo "Remove '${link_name}'"
+    rm -rf "$link_name"
+  fi
+  if [[ -e "$link_name" ]] || [[ -L "$link_name" ]]; then
+    echo "File '${link_name}' exists, nothing to do"
+    return 0
+  fi
+  echo "Create symlink '${link_name}'"
+  ln -s "$target" "$link_name"
+}
+
+init_collection() {
+  if [[ "$CLEAN" == "true" ]]; then
+    echo "Delete sqlite.db if exists"
+    rm -f sqlite.db
+    echo "Delete tdp_vars/tdp_cluster"
+    rm -rf inventory/tdp_vars/tdp_cluster
+  fi
+  tdp init
+}
+
+parse_cmdline "$@"
+
+tdp_monitoring_path='ansible_collections/alliage/tdp_monitoring'
+
+# Add tdp_monitoring topology to inventory
+create_symlink_if_needed "../../$tdp_monitoring_path/topology.ini" "inventory/topologies/monitoring"
+
+# Install Ansible roles and Python packages
+ansible-galaxy install -r "$tdp_monitoring_path/requirements.yml"
+pip install -r "ansible_collections/alliage/tdp_monitoring/requirements.txt"
+
+# Add tdp_monitoring path to TDP_COLLECTION_PATH
+sed -i "s|TDP_COLLECTION_PATH=\(.*\)\(:$tdp_monitoring_path\)*|TDP_COLLECTION_PATH=\1:$tdp_monitoring_path|" .env
+
+# Init collection (delete sqlite.db if needed)
+init_collection
