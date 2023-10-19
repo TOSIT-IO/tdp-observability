a31dda7228ef655f207817117de2406f66433d67
Jean-Marie Gervais <jean-marie.gervais@dgfip.finances.gouv.fr>
2023-10-19 14:03:15 +0200

    style: format output messages

--- dev/license_check.py
+++ dev/license_check.py
@@ -70,7 +70,8 @@ def license_check(files):
             file_list.sort()
         for file in file_list:
             if not file_ok(file, list(config[matcher])):
-                print(colored("missing copyright for",'red'), file.relative_to(p) )
+                print(colored("Copyright block missing or invalid.",'red'))
+                print(colored(file.relative_to(p), 'blue')+':1')
                 ret=1
     if ret == 0:
         print(colored("All files checked OK",'green'))
